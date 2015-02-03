f41e655c900d2fcb61392bde678c1ee12f7d26f7
Aram Kocharyan <akarmenia@gmail.com>
2015-02-03 22:56:34 +1100

    Added explicit export to both client and server. Version bump.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:tinycolor',
-  version: '1.1.0',
+  version: '1.1.0_1',
   summary: 'Fast, small color manipulation and conversion for JavaScript.',
   git: 'https://github.com/aramk/TinyColor.git#meteor'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.export('tinycolor');
+  api.export('tinycolor', ['client', 'server']);
   api.addFiles(['tinycolor.js'], ['client', 'server']);
 });
